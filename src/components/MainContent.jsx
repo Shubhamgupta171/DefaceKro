b48aeeadffe8564449199f7557d700fd5733ce8e
Shubham Gupta <inboxshubhamgupta17@gmail.com>
2026-03-19 22:46:55 +0530

    Update MainContent.jsx

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,14 +3,14 @@ import { Globe } from 'lucide-react';
 
 // You can change these image links to any external URL
 const INDIA_FLAG_URL = "/india_flag.png"; 
-const CYBER_HUB_URL = "https://i.pinimg.com/564x/cf/b0/d1/cfb0d1f5e27b5324adaa430dc65a6058.jpg";
+const CYBER_HUB_URL = "https://www.shutterstock.com/image-vector/title-australian-flag-icon-standard-600nw-2654303689.jpg";
 
 const MainContent = () => {
   return (
     <main className="main-content">
       <div className="banner">
         <h1 className="banner-title">
-          <img src="https://www.forumdaily.com/wp-content/uploads/2016/10/Depositphotos_65076917_m-2015.jpg" alt="India Flag" />
+          <img src="https://media.istockphoto.com/id/463470045/vector/skull-and-crossbones-icon.jpg?s=612x612&w=0&k=20&c=_yKpMc5ZMuIW8l6QV1vlNZKE5xGFD6JoxTbDwrnVyxA=" alt="India Flag" />
           Ministry of Defence
         </h1>
         <p className="banner-subtitle">
