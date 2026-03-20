10d7dc74bec44c07a806be0c0e9d47f4b372d453
Shubham Gupta <inboxshubhamgupta17@gmail.com>
2026-03-20 11:12:19 +0530

    Update MainContent.jsx

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,14 +3,14 @@ import { Globe } from 'lucide-react';
 
 // You can change these image links to any external URL
 const INDIA_FLAG_URL = "/india_flag.png"; 
-const CYBER_HUB_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Flag_of_India.png/1280px-Flag_of_India.png";
+const CYBER_HUB_URL = "https://i.pinimg.com/736x/f6/65/bf/f665bfb42f3ebae982c354225282d5f6.jpg";
 
 const MainContent = () => {
   return (
     <main className="main-content">
       <div className="banner">
         <h1 className="banner-title">
-          <img src="https://thumbs.dreamstime.com/b/united-states-department-army-london-uk-march-th-seal-symbol-portrayed-us-flag-da-one-three-military-depts-113157153.jpg" />
+          <img src="https://m.media-amazon.com/images/I/41DB6D1myoL.jpg_BO30,255,255,255_UF750,750_SR1910,1000,0,C_ZJPHNwYW4gZm9yZWdyb3VuZD0iIzBGMTExMSIgZm9udD0iQW1hem9uRW1iZXIgNjYiPjQuMzwvc3Bhbj4=,60,875,420,420,0,0_PIRIOFOURANDHALF-medium-V2,TopLeft,190,885_ZJPHNwYW4gZm9yZWdyb3VuZD0iIzU2NTk1OSIgZm9udD0iQW1hem9uRW1iZXIgNjYiPig3KTwvc3Bhbj4=,650,875,420,420,0,0_QL100_.jpg" />
           Ministry of Defence
         </h1>
         <p className="banner-subtitle">
