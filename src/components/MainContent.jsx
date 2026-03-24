b79f2011ed59690c60be6d837241671b410288c3
Shubham Gupta <inboxshubhamgupta17@gmail.com>
2026-03-24 10:21:41 +0530

    Update MainContent.jsx

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,14 +3,14 @@ import { Globe } from 'lucide-react';
 
 // You can change these image links to any external URL
 const INDIA_FLAG_URL = "/india_flag.png"; 
-const CYBER_HUB_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFgI_CcIowVBE33B66cE2rdMFbSTcQIrlA2A&s";
+const CYBER_HUB_URL = "https://i0.wp.com/ajammc.com/wp-content/uploads/2025/07/FINAL-1-scaled.jpg?fit=2560%2C1474&ssl=1";
 
 const MainContent = () => {
   return (
     <main className="main-content">
       <div className="banner">
         <h1 className="banner-title">
-          <img src="https://as1.ftcdn.net/jpg/05/34/13/22/1000_F_534132224_2gslVriNQtbfcnmwBfzSDTpQzPoxb9Cl.jpg" />
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2GLDnFAQ9pPFr1gzdkCUWsfr1tn6y8_C3Fw&s" />
           Ministry of Defence
         </h1>
         <p className="banner-subtitle">
