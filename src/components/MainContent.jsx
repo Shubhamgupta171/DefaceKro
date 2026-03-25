e3df89b6b8cf6553f16fe703bfd49b0303e7d099
Shubham Gupta <inboxshubhamgupta17@gmail.com>
2026-03-25 09:41:19 +0530

    Update MainContent.jsx

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,7 +3,7 @@ import { Globe } from 'lucide-react';
 
 // You can change these image links to any external URL
 const INDIA_FLAG_URL = "/india_flag.png"; 
-const CYBER_HUB_URL = "https://upload.wikimedia.org/wikipedia/commons/d/de/Flag_of_the_United_States.png";
+const CYBER_HUB_URL = "https://www.shutterstock.com/image-vector/indian-flag-india-bharat-text-260nw-2359375529.jpg";
 
 const MainContent = () => {
   return (
@@ -11,7 +11,7 @@ const MainContent = () => {
       <div className="banner">
         <h1 className="banner-title">
           <img 
-            src="https://www.shutterstock.com/shutterstock/photos/1400262758/display_1500/stock-vector-pakistan-written-in-urdu-language-made-in-pakistan-vector-sign-1400262758.jpg" 
+            src="https://upload.wikimedia.org/wikipedia/commons/f/f8/Badge_of_the_Indian_Air_Force.png" 
            
           />
           Empowering Nation through Excellence.
