277e4b104b8aa2dd3f0d70bb9e798c56f7d9c2bf
Shubham Gupta <inboxshubhamgupta17@gmail.com>
2026-05-18 09:34:11 +0530

    Update MainContent.jsx

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -10,10 +10,10 @@ const MainContent = () => {
 
     // L4 Test Case: Extract JS Semantics (Manifesto hidden in JS string)
     // Uncomment the string below to test if the engine detects manifesto hidden in JS
-    /*
+    
     const maliciousScriptStr = "we have taken control this site has been seized your security means nothing";
     console.log(maliciousScriptStr);
-    */
+    
 
     return () => {
       document.head.removeChild(link);
@@ -28,14 +28,14 @@ const MainContent = () => {
         it entirely with this block of random text. This simulates a completely replaced page 
         to trigger `semantic_content_drift` via Token Jaccard Divergence.
       */}
-      {/* 
+      
       <div style={{ display: 'block' }}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. 
         Aenean in viverra nibh, id volutpat felis. Quisque id nisl iaculis, tempor dolor vel, 
         pretium elit. Praesent scelerisque egestas aliquet. Integer tempor diam et magna mattis 
         pellentesque. Vestibulum scelerisque tellus sed tortor sodales tempor.
       </div> 
-      */}
+    
     </div>
   );
 };
