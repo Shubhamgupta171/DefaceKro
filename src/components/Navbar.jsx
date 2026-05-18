76bae804d3764f43afef1ee89df85388c6df92cf
Shubham Gupta <inboxshubhamgupta17@gmail.com>
2026-05-19 01:29:22 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,17 @@
-// import React from 'react';
+import React from 'react';
 
-// const Navbar = () => {
-//   return (
-//     <nav className="top-navbar">
-//       <ul>
-//         <li><a href="#">HOME</a></li>
-//         <li><a href="#">ABOUT ME</a></li>
-//         <li><a href="#">ADMIN Panel</a></li>
-//         <li><a href="#">API Status</a></li>
-//         <li><a href="#">Register</a></li>
-//       </ul>
-//     </nav>
-//   );
-// };
+const Navbar = () => {
+  return (
+    <nav className="top-navbar">
+      <ul>
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">ABOUT ME</a></li>
+        <li><a href="#">ADMIN Panel</a></li>
+        <li><a href="#">API Status</a></li>
+        <li><a href="#">Register</a></li>
+      </ul>
+    </nav>
+  );
+};
 
-// export default Navbar;
+export default Navbar;
